5f4f892d4c11e40b3f2123440a66e8c897dc93d4
PierreSQS <nomo_messina@web.de>
2021-04-05 13:39:15 +0200

    Chap47: added POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PetType{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
