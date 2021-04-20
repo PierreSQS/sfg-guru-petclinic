ca5f40ae728fb970f601188998a7da5097dac7b5
PierreSQS <nomo_messina@web.de>
2021-04-20 17:28:49 +0200

    Chap127: Fixed Broken Menu Links. Closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,8 +4,10 @@ import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
     private final OwnerService ownerService;
 
@@ -13,9 +15,14 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping({"/owners/index","/owners/index.html","/owners"})
+    @GetMapping({"/index","/index.html",""})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/find")
+    public String findOwner(Model model) {
+        return "notyetimplemented";
     }
 }
