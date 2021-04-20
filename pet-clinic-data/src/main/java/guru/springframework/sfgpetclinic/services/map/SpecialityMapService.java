eb71248bbe53fcc0618b278e8db301c6d125a957
PierreSQS <nomo_messina@web.de>
2021-04-20 13:19:43 +0200

    Chap125: Implemented Speciality Map
    Service. Closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+import java.util.List;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+}
