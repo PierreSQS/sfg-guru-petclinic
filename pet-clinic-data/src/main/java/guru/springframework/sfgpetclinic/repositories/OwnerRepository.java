7e3b141d1166899bb68ab19b5bbba87d608c48a4
PierreSQS <nomo_messina@web.de>
2021-05-10 19:08:46 +0200

    Chap151: Created Spring Data JPA Service for owners Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+    List<Owner> findByLastName(String lastName);
 }
