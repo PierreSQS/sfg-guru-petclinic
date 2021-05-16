7e874851d821a0fcea8c1965681b5b102acf70d7
PierreSQS <nomo_messina@web.de>
2021-05-16 15:33:50 +0200

    Chap187: Added a Test for the OwnerController. Closes #53

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,31 +1,79 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@WebMvcTest(controllers = {OwnerController.class})
+@ExtendWith(SpringExtension.class)
 class OwnerControllerTest {
 
-    @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     OwnerService ownerServiceMock;
 
-    @Test
-    void listOwners() throws Exception {
-        mockMvc.perform(get("/owners/index")).andDo(print());
+    @InjectMocks
+    OwnerController ownerController;
+
+    @BeforeEach
+    void setUp() {
+        ownerController = new OwnerController(ownerServiceMock);
+        mockMvc = standaloneSetup(ownerController).build();
+
+        Owner owner1 = new Owner();
+        owner1.setFirstName("owner1");
+        Owner owner2 = new Owner();
+        owner2.setFirstName("owner2");
+
+        List<Owner> owners = Arrays.asList(owner1, owner2);
+        when(ownerServiceMock.findAll()).thenReturn(owners);
     }
 
     @Test
-    void findOwner() {
+    void listOwners() throws Exception {
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owners"))
+                .andExpect(model().attribute("owners",ownerServiceMock.findAll()))
+                .andExpect(view().name("owners/index"))
+                .andDo(print());
+    }
+
+    @Test
+    void listOwnersByIndexPage() throws Exception {
+        mockMvc.perform(get("/owners/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owners"))
+                .andExpect(model().attribute("owners",hasSize(2)))
+                .andExpect(header().string("Content-Language","en"))
+                .andExpect(view().name("owners/index"))
+                .andDo(print());
+    }
+
+    @Test
+    void findOwner() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("owner"))
+                .andExpect(view().name("notyetimplemented"));
+        verifyZeroInteractions(ownerServiceMock);
     }
 }
