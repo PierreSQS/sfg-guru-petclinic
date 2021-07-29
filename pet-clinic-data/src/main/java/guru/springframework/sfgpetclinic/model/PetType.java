f7c6b6b5e6e97223f8c0b696a5a1e94d8423a298
PierreSQS <nomo_messina@web.de>
2021-07-29 03:06:10 +0200

    added Builder Pattern to PetType. #57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,23 +1,30 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+/**
+ * Modified by Pierrot on 7/29/21.
+ */
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pettypes")
 public class PetType extends BaseEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        this.name = name;
+    }
 
     private String name;
 
     @Override
     public String toString() {
-        return "PetType{" +
-                "name='" + name + '\'' +
-                '}';
+        return name;
     }
 }
