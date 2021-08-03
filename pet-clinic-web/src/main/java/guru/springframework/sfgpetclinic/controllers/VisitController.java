5733e1c7a514c04ca3674d660031cb5638543fea
PierreSQS <nomo_messina@web.de>
2021-08-03 03:18:37 +0200

    Chap222: Added Controller and Tests for Visit Init Creation Form

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("owners/{ownerId}/pets/{petId}")
+public class VisitController {
+
+    private static final String VIEWS_VISITS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateVisitForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final VisitService visitService;
+
+    public VisitController(PetService petService, OwnerService ownerService,
+                           VisitService visitService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.visitService = visitService;
+    }
+
+    @InitBinder
+    public void setDisallowedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @ModelAttribute("pet")
+    public Pet findPet(@PathVariable("petId") Long id) {
+        return petService.findById(id);
+    }
+
+    @GetMapping("visits/new")
+    public String initCreateVisit(Visit visit, ModelMap modelMap) {
+        visit = new Visit();
+        modelMap.put("visit", visit);
+        return VIEWS_VISITS_CREATE_OR_UPDATE_FORM;
+    }
+
+}
---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,73 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = {VisitController.class})
+class VisitControllerTest {
+
+    private Owner owner;
+    private Pet pet;
+
+    @MockBean
+    VisitService visitSrvMock;
+
+    @MockBean
+    OwnerService ownerSrvMock;
+
+    @MockBean
+    PetService petSrvMock;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owner = new Owner();
+        owner.setId(1L);
+        owner.setFirstName("Owner1");
+        owner.setLastName("Mock");
+
+        pet = Pet.builder().id(1L).name("Fluppy").build();
+        pet.setOwner(owner);
+        pet.setPetType(PetType.builder().name("Dog").build());
+        System.out.println(pet);
+
+        when(ownerSrvMock.findById(anyLong())).thenReturn(owner);
+        when(petSrvMock.findById(anyLong())).thenReturn(pet);
+    }
+
+    @Test
+    void initCreateVisit() throws Exception {
+        mockMvc.perform(get("/owners/{ownerId}/pets/{petId}/visits/new"
+                            ,owner.getId(),pet.getId()))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("pet",hasProperty("owner")))
+                .andExpect(view().name("pets/createOrUpdateVisitForm"))
+                .andExpect(content().string(containsString("Fluppy")))
+                .andDo(print());
+    }
+}
